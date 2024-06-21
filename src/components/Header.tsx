602c2dd7766cfaa0105d23d8dd8deab3d5ac6f58
kinakomoch7 <book-insect14@outlook.jp>
2024-06-22 00:41:19 +0900

    [fix]テキスト色固定

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 
 export const Header = () => {
   return (
-    <div className="w-full bg-white p-3 py-5 text-2xl font-semibold border-b-2">
+    <div className="w-full bg-white p-3 py-5 text-2xl font-semibold border-b-2 text-black">
       マイナビDB
     </div>
   )
--- src/components/ListView.tsx
+++ src/components/ListView.tsx
@@ -19,7 +19,7 @@ export const ListView = () => {
           <a href={job.link} className="text-sky-500 hover:text-sky-300">{job.name}</a>
           <div className="flex flex-col md:flex-row justify-between">
             <p className="bg-zinc-300 text-zinc-600">{job.industry}</p>
-            <p>{job.deadline}</p>
+            <p className="text-black">{job.deadline}</p>
           </div>
         </div>
       ))}
