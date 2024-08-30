d1cf5edae1de669d5a260b46e69b57fc5f303a2f
k_tomoki <114901771+kinakomoch7@users.noreply.github.com>
2024-08-30 15:59:55 +0900

    [add]コミット漏れ

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,8 @@ export default async function Home(){
 
   const res = await fetch(`${process.env.HOME_URL}/api/getJobList`, {
     next: {
-      revalidate:  604800 // 1週間（秒単位）
+      revalidate: 86400 // 1日（秒単位）
+      // revalidate:  604800 // 1週間（秒単位）
     }
   })
   const data = await res.json()
